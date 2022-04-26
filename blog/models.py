239cee0c72bf6e2a32cd2fe198cf298bc1c1e165
snyderae13 <snyderae13@icloud.com>
2022-04-26 18:02:27 -0500

    blog post database created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# a class for the blog post that will be a model class 
+# https://docs.djangoproject.com/en/4.0/ref/models/instances/#django.db.models.Model
+
+# a reference for the field types https://docs.djangoproject.com/en/4.0/ref/models/fields/
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    #auto now add will add a date and time once the user clicks save
+    slug = models.SlugField(max_length=100, unique=True)
+    # this is to make sure that each article has a unique /name
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    # this sets the many to one relationship. and cascade will allow once a user is delete that their posts are deleted as well. 
+    
