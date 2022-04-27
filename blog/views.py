b4a604696acdb2d5696b31d3d37d637a11a1571d
snyderae13 <snyderae13@icloud.com>
2022-04-27 16:14:43 -0500

    added urls folder in blog

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from pyexpat import model
 from django.shortcuts import render
 from .models import Post
+from django.views import generic
 
 # Create your views here.
 # view docs : https://docs.djangoproject.com/en/4.0/ref/class-based-views/base/
 
 
-class BlogView:
+class BlogView(generic.DetailView):
     model = Post
     template_name = 'blog.html'
