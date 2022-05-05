19ea493d5def1a1fc85513954f659eeaecc2dc01
snyderae13 <snyderae13@icloud.com>
2022-05-05 10:30:42 -0500

    added files and changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,3 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
